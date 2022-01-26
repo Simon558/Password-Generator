2bcff1db5f604b9ca0b22cc57868a0dddd957f99
Simon <SimonDutkaWork@gmail.com>
2022-01-26 15:07:19 +0100

    Addded: Copy button

--- src/script.js
+++ src/script.js
@@ -8,3 +8,18 @@ sliderRange.addEventListener('keydown', changeValue);
 function changeValue() {
     sliderValue.innerHTML = sliderRange.value;
 }
+
+const passwordInput = document.getElementById('password-input');
+const btnCopy = document.getElementById('btn-copy');
+
+//Coping password to clipboard after click copy button
+btnCopy.addEventListener('click', copyPassword);
+
+function copyPassword() {
+    let copiedPassword = passwordInput.value;
+
+    passwordInput.select();
+    navigator.clipboard.writeText(copiedPassword);
+
+    btnCopy.innerHTML = 'Copied';
+}
